462c4a467d82af52bb3e193831d5a8f15bfa52cf
Marcelo <marcelococcaro@email.com>
2024-06-07 21:52:01 -0300

    Adicionando exemplo 2

---
+++ exemplo2.py
@@ -0,0 +1,15 @@
+x = 0
+
+while (x < 10):
+
+    numero = int(input("Digite um número:"))
+
+    if (numero % 2 == 0):
+
+        print("O número {0} é par".format(numero))
+
+    else:
+
+        print("O número {0} é ímpar".format(numero))
+
+    x += 1
